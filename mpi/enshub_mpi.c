91bc18410f5d7522bd46811b08be5ae88b37ee3d
uenop <dummy@mail.com>
2011-09-15 16:23:22 +0900

    add

--- mpi/enshub_mpi.c
+++ mpi/enshub_mpi.c
@@ -6,28 +6,71 @@
 #define NY 193
 
 int main(int argc, char** argv){
-    double u[NY+1][NX+1], un[NY+1][NX+1];
+    int irank,nrank;
+
+    MPI_Init (&argc, &argv);
+    MPI_Comm_size (MPI_COMM_WORLD, &nrank);
+    MPI_Comm_rank (MPI_COMM_WORLD, &irank);
+
+    int height=(NY-1)/nrank+2, width=NX+1;
+    
+    double (*u)[width];
+    u=(double(*)[width])malloc(height*width*sizeof(double));
+    u=(double(*)[width])(&u[1][1]);
+
+    double (*un)[width];
+    un=(double(*)[width])malloc(height*width*sizeof(double));
+    un=(double(*)[width])(&un[1][1]);
+
     double h = 1.0/NX;
     double dt = 0.1*h*h;
     double dth2 = dt/h/h;
     FILE *udata;
     int i,j,k;
 
-    for(j=0;j<=NY;j++){         //配列の初期化
-        for(i=0;i<=NX;i++){
-            if(i==0){
-                u[j][i] = 0.5;
-                un[j][i] = 0.5;
-            }else if(j==0){
-                u[j][i] = 1.0;
-                un[j][i] = 1.0;
-            }else{
-                u[j][i] = 0.0;
-                un[j][i] = 0.0;
+    for(j=-1;j<height-1;j++)
+        for(i=-1;i<width-1;i++)
+            u[j][i] = 0.0;
+
+    if( irank==0 ){
+        // (y=0)
+        for(i=-1;i<width-1;i++)
+            u[-1][i] = 1.0;
+    }
+    for(j=0;j<height-1;j++)
+        u[j][-1] = 0.5;
+
+
+    /* print */
+
+    if (irank == 0){
+        udata = fopen("u.data","w");
+        for(j=-1;j<height-2;j++){
+            for(i=-1;i<width-1;i++){
+                fprintf( udata, "%.15E %.15E %.15E\n", (i+1)*h, (j+1)*h, u[j][i] );
             }
         }
+        for(int r=1;r<nrank-1;r++){
+            //MPI_Recv (&buf,count,datatype,source,tag,comm,&status)
+            MPI_Recv( &u[0][-1], width*(height-2), MPI_DOUBLE, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+            for(j=0;j<height-2;j++){
+                for(i=-1;i<width-1;i++){
+                    fprintf( udata, "%.15E %.15E %.15E\n", (i+1)*h, (j+1+r*(height-2))*h, u[j][i] );
+                }
+            }
+        }
+        MPI_Recv( &u[0][-1], width*(height-1), MPI_DOUBLE, nrank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+        for(j=0;j<height-1;j++)
+            for(i=-1;i<width-1;i++)
+                fprintf( udata, "%.15E %.15E %.15E\n", (i+1)*h, (j+1+(nrank-1)*(height-2))*h, u[j][i] );
+        fclose(udata);
+    } else if (irank<nrank-1) {
+        MPI_Send (&u[0][-1], width*(height-2), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD );
+    } else {
+        MPI_Send (&u[0][-1], width*(height-1), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD );
     }
 
+/*
     for(k=0;k<40000;k++){       // 40000 times loop
         for(j=1;j<NY;j++){
             for(i=1;i<NX;i++){
@@ -51,7 +94,8 @@ int main(int argc, char** argv){
         }
         fprintf(udata,"\n");
     }
-    fclose(udata);
+*/
+    MPI_Finalize ();
 
     return 0;
 }
