d46c171b4b499bfc60b5d865ad0fc73871cb8f3d
uenop <dummy@mail.com>
2011-09-20 14:19:44 +0900

    indent

--- mpi2/enshub_mpi2.c
+++ mpi2/enshub_mpi2.c
@@ -95,34 +95,34 @@ int main(int argc, char** argv){
     /* PRINT */
 
     /* 西端にgatherしてprint */
-   MPI_Comm row;
-   MPI_Cart_coords(cart, irank, 2, c);
-   MPI_Comm_split(cart, py, px, &row);
+    MPI_Comm row;
+    MPI_Cart_coords(cart, irank, 2, c);
+    MPI_Comm_split(cart, py, px, &row);
 
 // MPI_Gather (&sendbuf,sendcnt,sendtype,&recvbuf, 
 //             recvcount,recvtype,root,comm)
 //   double recvbuf[ny+2][(nx+2)*dims[1]];
-   double recvbuf[(ny+2)*dims[0]][(nx+2)];
-   int coord[2] = {py, 0};
-   printf("rank: %d, coord: %d %d, c:%d %d\n", irank, coord[1], coord[0], c[1], c[0]);
-   MPI_Gather(&u[-1][-1], (nx+2)*(ny+2), MPI_DOUBLE,
-              recvbuf, (nx+2)*(ny+2), MPI_DOUBLE, 0, row);
+    double recvbuf[(ny+2)*dims[0]][(nx+2)];
+    int coord[2] = {py, 0};
+    printf("rank: %d, coord: %d %d, c:%d %d\n", irank, coord[1], coord[0], c[1], c[0]);
+    MPI_Gather(&u[-1][-1], (nx+2)*(ny+2), MPI_DOUBLE,
+               recvbuf, (nx+2)*(ny+2), MPI_DOUBLE, 0, row);
 
-   FILE *udata;
+    FILE *udata;
 
-   for(int pj=0; pj<dims[0]; pj++){
-       if(px==0){
-           udata = fopen("u.data","w");
-           for(j=1; j<ny+1; j+=4){
-               for(int pi=0; pi<dims[1]; pi++)
-                   for(i=1; i<nx+1; i+=4)
-                       fprintf( udata, "%.15E %.15E %.15E\n", (i + nx*pi)*h, (j + ny*pj)*h, u[j + pi*ny][i] );
-               fprintf( udata, "\n" );
-           }
-           fclose(udata);
-       }
-       MPI_Barrier(MCW);
-   }
+    for(int pj=0; pj<dims[0]; pj++){
+        if(px==0){
+            udata = fopen("u.data","w");
+            for(j=1; j<ny+1; j+=4){
+                for(int pi=0; pi<dims[1]; pi++)
+                    for(i=1; i<nx+1; i+=4)
+                        fprintf( udata, "%.15E %.15E %.15E\n", (i + nx*pi)*h, (j + ny*pj)*h, u[j + pi*ny][i] );
+                fprintf( udata, "\n" );
+            }
+            fclose(udata);
+        }
+        MPI_Barrier(MCW);
+    }
 
 //    MPI_File_close(&udata);
     MPI_Finalize ();
--- omp/prob2.c
+++ omp/prob2.c
@@ -4,64 +4,72 @@
 #define N 10
 
 int main(int ac, char* av){
-	int i,j;
-	int a[N+1],b[N+1],c[N+1][N+1],f[N+1],x[N+1],y[N+1];
-	int sum;
-	int num_threads = omp_get_num_threads();
-	int myid;
-	
-	// (0)
-	for(i=0;i<N+1;i++)
-		a[i]=b[i]=f[i]=x[i]=y[i] =i;
-	printf("MAX threads:%d\n", omp_get_max_threads());
+    int i,j;
+    int a[N+1],b[N+1],c[N+1][N+1],f[N+1],x[N+1],y[N+1];
+    int sum;
+    int num_threads = omp_get_num_threads();
+    int myid;
+    
+    // (0)
+    for(i=0;i<N+1;i++)
+        a[i]=b[i]=f[i]=x[i]=y[i] =i;
+    printf("MAX threads:%d\n", omp_get_max_threads());
 #pragma single
-	{
-	for(i=0;i<N;i++)
-		printf("i:%d a:%d b:%d x:%d\n", i, a, b, x);
-	}
-	// (1)
+    {
+        for(i=0;i<N;i++)
+            printf("i:%d a:%d b:%d x:%d\n", i, a, b, x);
+    }
 #pragma omp parallel private(myid)
-	{
-		myid = omp_get_thread_num();
+    {
+        // (1)
+        // a[1] = b[0] + c[1];
+        // for(i=2;i<N;i++){
+        //   b[i-1] = f[i-1];
+        //   a[i] = b[i-1] +c[i];
+        // }
+           
+        myid = omp_get_thread_num();
 #pragma omp for schedule(static,2)
-	for(i=1;i<N;i++){
-		b[i] = f[i];
-		a[i] = b[i-1] + x[i];
-	}
-	
-	printf("\n\n");
-	// (2)
+        for(i=1;i<N;i++){
+            b[i] = f[i];
+            a[i] = b[i-1] + x[i];
+        }
+    
+        printf("\n\n");
+        // (2)
+        // jで並列化はダメ(依存)
 #pragma omp for private (j)
-	for(i=0;i<N;i++)
-		for(j=0;j<N;j++)
-			x[i] = x[i] + c[i][j] + b[j];
+        for(i=0;i<N;i++)
+            for(j=0;j<N;j++)
+                x[i] = x[i] + c[i][j] + b[j];
 
-	// (3)
+        // (3)
+        // schedule(static,1) //fortranなので1,3,5がthread0
 
-//	myid = 1 - myid;
-	for(i=myid+1; i<N; i+=2){
-		printf("i: %d, myid:%d\n", i, myid);
-		a[i] = b[i];
-	}
+//  myid = 1 - myid;
+        for(i=myid+1; i<N; i+=2){
+            printf("i: %d, myid:%d\n", i, myid);
+            a[i] = b[i];
+        }
 
-	// (4)
+        // (4)
 #pragma omp for schedule(static,1)
-	for(i=2;i<N;i++)
-		a[i] = a[i-2] + b[i];
+        for(i=2;i<N;i++)
+            a[i] = a[i-2] + b[i];
 
-	// (5)
-	sum=0;
+        // (5)
+        sum=0;
 #pragma omp for reduction(+:sum)
-	for(i=0;i<N;i++)
-		sum = sum+a[i]*b[i];
+        for(i=0;i<N;i++)
+            sum = sum+a[i]*b[i];
 
-	// (6) Stencilの計算
+        // (6) Stencilの計算
 #pragma omp for private(j)
-	for(i=0;i<N;i++)
-		for(j=1;j<N;j++)
-			c[i][j] = c[i][j] + c[i][j-1];
-	} // END OMP PARALLEL
+        for(i=0;i<N;i++)
+            for(j=1;j<N;j++)
+                c[i][j] = c[i][j] + c[i][j-1];
+    } // END OMP PARALLEL
 
 
-	return 0;
+    return 0;
 }
