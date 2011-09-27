f244e5ece68ced2dec971923b86ac053fc20e002
uenop <dummy@mail.com>
2011-09-27 14:15:09 +0900

    fix

--- mpi2/enshub_mpi2.c
+++ mpi2/enshub_mpi2.c
@@ -97,7 +97,7 @@ int main(int argc, char** argv){
 
     /* PRINT */
     /* コピペ */
-    fprintf(stderr, "%d: checkpoint1\n", irank);
+//    fprintf(stderr, "%d: checkpoint1\n", irank);
     
 //    printf("rank: %d, c: %d %d\n", irank, c[1], c[0]);
     MPI_File udata;
@@ -121,10 +121,10 @@ int main(int argc, char** argv){
     if (px == dims[1]-1) subsize[1]+=LW+1;      /* 東端→ */
 
     MPI_Datatype ftype;
-    fprintf(stderr, "%d: checkpoint2\n", irank);
+//    fprintf(stderr, "%d: checkpoint2\n", irank);
     MPI_Type_create_subarray(2, size, subsize, start,
                              MPI_ORDER_C, MPI_CHAR, &ftype);
-    fprintf(stderr, "%d: checkpoint3\n", irank);
+//    fprintf(stderr, "%d: checkpoint3\n", irank);
     MPI_Type_commit(&ftype);
     MPI_File_set_view(udata, 0, MPI_CHAR, ftype, "native",
                       MPI_INFO_NULL);
