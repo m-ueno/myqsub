9d36dec2e9c4482cb34d4a05abb6fb1d08888fd9
uenop <dummy@mail.com>
2011-09-27 15:01:03 +0900

    fix

--- mpi2/enshub_mpi2.c
+++ mpi2/enshub_mpi2.c
@@ -106,18 +106,15 @@ int main(int argc, char** argv){
     MPI_File_set_size(udata,0);
 
     int size[2] = {NY+1, LW*(NX+1)+1}, subsize[2], start[2];
-// ...
-    MPI_Dims_create(nrank, 2, dims);
-    MPI_Cart_coords(cart, irank, 2, c);
 // ... py,pxはcartesian座標
     subsize[0] = ny;
     subsize[1] = LW*nx;
-    start[0] = py*ny;         /* py==0 => 1 */
+    start[0] = py*ny+1;         /* py==0 => 1 */
     start[1] = LW*(px*nx+1);    /* px==0 => LW */
 
-    if (py == 0){ subsize[0]++; start[0]=-1; }   /* 南端↓ */
+    if (py == 0){ subsize[0]++; start[0]=0; }   /* 南端↓ */
     if (py == dims[0]-1) subsize[0]++;          /* 北端↑ */
-    if (px == 0){ subsize[1]+=LW; start[1]=-1; } /* 西端← */
+    if (px == 0){ subsize[1]+=LW; start[1]=0; } /* 西端← */
     if (px == dims[1]-1) subsize[1]+=LW+1;      /* 東端→ */
 
     MPI_Datatype ftype;
