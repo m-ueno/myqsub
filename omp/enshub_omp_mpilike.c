b42fcdc35f0bb18e077e5ffb818cef00b82b520f
uenop <dummy@mail.com>
2011-09-28 17:47:07 +0900

    fix

--- omp/enshub_omp_mpilike.c
+++ omp/enshub_omp_mpilike.c
@@ -20,47 +20,41 @@ int main(int argc, char *argv[]){
 
 #pragma omp parallel private (k)
     {
-        nrank = omp_get_num_threads();
-        irank = omp_get_thread_num();
+        nrank = omp_get_num_threads(); /* 4 */
+        irank = omp_get_thread_num();  /* [0,3] */
+        int nx = (NX-1)/nrank;         /* 48 = 192/4 */
+        int ny = (NY-1)/nrank;         /* 48 */
+        int north=0,south=0;
+        if(irank==0) south=-1;   /* 南端-1 */
+        if(irank==nrank-1) north=1; /* 北端+1 */
 
-        printf("i:%d, n:%d\n", irank, nrank);
-
-#pragma omp for private (i)
-        for(j=0;j<=NY;j++){
-            for(i=0;i<=NX;i++){
-                if (i==0) {
+        for(j=irank*ny+1+south; j<(irank+1)*ny+north; j++){
+            for(i=0; i<NX+1; i++){
+                if (i==0)
                     u[j][i] = 0.5;
-                } else if(j==0) {
+                else if (j==0)
                     u[j][i] = 1.0;
-                } else {
+                else
                     u[j][i] = 0.0;
-                }
             }
         }
 
         for(k=0;k<40000;k++){       // 40000 times loop
-#pragma omp for private (i)
-            for(j=1;j<NY;j++){
-                for(i=1;i<NX;i++){
+            for(j=irank*ny+1; j<(irank+1)*ny+1; j++) /* 手動で分割 */
+                for(i=1;i<NX;i++)
                     un[j][i] = u[j][i] + ( -4*u[j][i] + u[j][i+1] + u[j][i-1] + u[j+1][i] + u[j-1][i] )*dth2;
-                }
-            }
-#pragma omp for private (i)
-            for(j=1;j<NY;j++){
-                for(i=1;i<NX;i++){
+            for(j=irank*ny+1; j<(irank+1)*ny+1; j++)
+                for(i=1;i<NX;i++)
                     u[j][i] = un[j][i];
-                }
-            }
         } // end for(k)
     }   // end parallel
 
-    if( (udata = fopen("u.data","w")) == NULL ){
+    if( (udata = fopen("u_mpilike.data","w")) == NULL ){
         printf("Can't open file.\n");
     }
     for(j=0;j<=NY;j++){//ファイル出力
-        for(i=0;i<=NX;i++){
+        for(i=0;i<=NX;i++)
             fprintf( udata, "%.15E %.15E %.15E\n", i*h, j*h, u[j][i] );
-        }
         fprintf(udata,"\n");
     }
     fclose(udata);
@@ -69,5 +63,4 @@ int main(int argc, char *argv[]){
     printf("t2-t1: %f\n", t2-t1);
 
     return 0;
-
 }
