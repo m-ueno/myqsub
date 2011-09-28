e7e98355a59d9aa6896f0d55661fa4032806d419
uenop <dummy@mail.com>
2011-09-28 16:43:57 +0900

    fix

--- mpi/run-mpi4.sh
+++ mpi/run-mpi4.sh
@@ -2,24 +2,17 @@
 #  "#"で始まる行はコメント。ただし、
 #  "# "に"@"と"$"が続く行は、NQSオプションの指定
 #========== NQSオプション ==========
+NUM_PROCS=4
 # @$-eo
 #
 # キューの指定
 # @$-q sh20100
-# @$-lP 4
+# @$-lP $NUM_PROCS
 # @$-lp 1
 # @$-lm 1800mb
 # @$-cp 0:10:00
 
 set -x
 cd $QSUB_WORKDIR
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
-mpiexec -n 4 a.out
+mpiexec -n $NUM_PROCS a.out
+mpiexec -n $NUM_PROCS a.out
