b0ffa51ec105e013266b86faf3e8c0a62bfd4b4b
a0064164 <dummy@mail.com>
2011-09-28 15:43:49 +0900

    fix

--- chikuji/hibino.c
+++ chikuji/hibino.c
@@ -16,13 +16,13 @@ int main(int argc, char *argv[]){
 
     for(j=0;j<=NY;j++){         //配列の初期化
         for(i=0;i<=NX;i++){
-            if(i==0){
+            if (i==0) {
                 u[j][i] = 0.5;
                 un[j][i] = 0.5;
-            }else if(j==0){
+            } else if (j==0) {
                 u[j][i] = 1.0;
                 un[j][i] = 1.0;
-            }else{
+            } else {
                 u[j][i] = 0.0;
                 un[j][i] = 0.0;
             }
@@ -30,25 +30,20 @@ int main(int argc, char *argv[]){
     }
 
     for(k=0;k<40000;k++){       // 40000 times loop
-        for(j=1;j<NY;j++){
-            for(i=1;i<NX;i++){
+        for(j=1;j<NY;j++)
+            for(i=1;i<NX;i++)
                 un[j][i] = u[j][i] + ( -4*u[j][i] + u[j][i+1] + u[j][i-1] + u[j+1][i] + u[j-1][i] )*dth2;
-            }
-        }
-        for(j=1;j<NY;j++){
-            for(i=1;i<NX;i++){
+        for(j=1;j<NY;j++)
+            for(i=1;i<NX;i++)
                 u[j][i] = un[j][i];
-            }
-        }
     }
 
-    if((udata = fopen("u.data","w"))==NULL){
+    if( (udata = fopen("u.data","w"))==NULL ){
         printf("ファイルを開けません。\n");
     }
     for(j=0;j<=NY;j++){//ファイル出力
-        for(i=0;i<=NX;i++){
+        for(i=0;i<=NX;i++)
             fprintf(udata,"%.15E %.15E %.15E\n",i*h,j*h,u[j][i]);
-        }
         fprintf(udata,"\n");
     }
     fclose(udata);
