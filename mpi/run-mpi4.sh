69cae0b5069283cff76e1584eef409ef5fd29100
a0064164 <dummy@mail.com>
2011-09-28 16:47:41 +0900

    fix

--- mpi/run-mpi4.sh
+++ mpi/run-mpi4.sh
@@ -1,18 +1,14 @@
-# nqs_mpi_fujitsu.sh
-#  "#"で始まる行はコメント。ただし、
 #  "# "に"@"と"$"が続く行は、NQSオプションの指定
-#========== NQSオプション ==========
-NUM_PROCS=4
 # @$-eo
 #
-# キューの指定
 # @$-q sh20100
-# @$-lP $NUM_PROCS
+# @$-lP 4
 # @$-lp 1
 # @$-lm 1800mb
 # @$-cp 0:10:00
 
 set -x
 cd $QSUB_WORKDIR
+NUM_PROCS=4
 mpiexec -n $NUM_PROCS a.out
 mpiexec -n $NUM_PROCS a.out
--- omp/run-omp4.sh
+++ omp/run-omp4.sh
@@ -4,11 +4,11 @@
 # キューの指定
 # @$-q sh20100
 # @$-lP 1
-# @$-lp 16
+# @$-lp 4
 # @$-lm 1800mb
 # @$-cp 0:10:00
 set -x
-OMP_NUM_THREADS=16
+OMP_NUM_THREADS=4
 export OMP_NUM_THREADS
 
 cd $QSUB_WORKDIR
