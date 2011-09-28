ca899d36f3e3ad52e65f5b49e3f40019ae135b5f
a0064164 <dummy@mail.com>
2011-09-28 16:32:14 +0900

    fix

--- mpi/run-mpi4.sh
+++ mpi/run-mpi4.sh
@@ -1,24 +1,25 @@
 # nqs_mpi_fujitsu.sh
-#
-# 富士通コンパイラ+プロセス並列用(MPI, XPFortran)
-#
 #  "#"で始まる行はコメント。ただし、
 #  "# "に"@"と"$"が続く行は、NQSオプションの指定
-#
 #========== NQSオプション ==========
-#
 # @$-eo
 #
 # キューの指定
 # @$-q sh20100
-# @$-lP 1
-# @$-lp 16
+# @$-lP 4
+# @$-lp 1
 # @$-lm 1800mb
 # @$-cp 0:10:00
+
 set -x
-OMP_NUM_THREADS=16
-export OMP_NUM_THREADS
-
 cd $QSUB_WORKDIR
-./a.out
-
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
+mpiexec -n 4 a.out
