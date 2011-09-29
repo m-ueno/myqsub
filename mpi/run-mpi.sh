af436363a035133b227087c435891a27a5e4210c
a0064164 <dummy@mail.com>
2011-09-30 04:10:34 +0900

    fix

--- mpi/run-mpi.sh
+++ mpi/run-mpi.sh
@@ -10,7 +10,8 @@ cd $QSUB_WORKDIR
 echo $QSUB_WORKDIR
 #cd /home/a/a0064164/work/EnshuB/mpi
 
-NUM_CPUS=(2 4 8 12 16)
+#NUM_CPUS=(2 4 8 12 16)
+NUM_CPUS=(1)
 for i in ${NUM_CPUS[@]}; do
     count=10
     while [ $count -gt 0 ]; do
