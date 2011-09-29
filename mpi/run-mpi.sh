df2b632903ae1f0c9bccc66b647d4dbfcf37e0e0
a0064164 <dummy@mail.com>
2011-09-29 15:27:04 +0900

    work mpi script

--- mpi/run-mpi.sh
+++ mpi/run-mpi.sh
@@ -1,18 +1,20 @@
-#  "# "に"@"と"$"が続く行は、NQSオプションの指定
 # @$-eo
-# @$-q sh20100
+# @$-q gh10034
+# @$-g gh10034
 # @$-lP 16
 # @$-lp 1
 # @$-lm 1800mb
 # @$-cp 0:10:00
 
 cd $QSUB_WORKDIR
-NUM_CPUS=(1 2 4 8 12 16)
+echo $QSUB_WORKDIR
+#cd /home/a/a0064164/work/EnshuB/mpi
+
+NUM_CPUS=(2 4 8 12 16)
 for i in ${NUM_CPUS[@]}; do
     count=10
     while [ $count -gt 0 ]; do
-        mpiexec -n $i a.out
+        mpiexec -n $i ./a.out
         count=`expr $count - 1`
     done
 done
-
