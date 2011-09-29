95f57736b45db66aa076d9fa421e716a1b974ebd
uenop <dummy@mail.com>
2011-09-29 17:50:52 +0900

    fix

--- omp/run-omp.sh
+++ omp/run-omp.sh
@@ -7,14 +7,14 @@
 # @$-cp 0:10:00
 
 cd $QSUB_WORKDIR
-cat $QSUB_WORKDIR
 PROGS=("enshub_omp" "enshub_omp_with_first_touch" "enshub_omp_mpilike")
 NUM_THREADS=(1 2 4 8 12 16)
 for prog in ${PROGS[@]}; do
     echo $prog
     for i in ${NUM_THREADS[@]}; do
-       	echo $i threads
-        export OMP_NUM_THREADS=i
+        echo $i threads
+        OMP_NUM_THREADS=$i
+        export OMP_NUM_THREADS
         count=3
         while [ $count -gt 0 ]; do
             ./$prog
