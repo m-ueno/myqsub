d8f97d68d5f43c757d3c32950b11d0c706cfea20
uenop <dummy@mail.com>
2011-09-29 16:42:35 +0900

    fix

--- omp/run-omp.sh
+++ omp/run-omp.sh
@@ -1,4 +1,14 @@
-# runomp
+# run-omp
+# @$-eo
+# @$-q gh10034
+# @$-g gh10034
+# @$-lP 1
+# @$-lp 16
+# @$-lm 1800mb
+# @$-cp 0:10:00
+
+cd $QSUB_WORKDIR
+
 #set -x
 PROGS=("enshu_omp" "enshu_omp_with_firsttouch" "enshu_omp_mpilike")
 NUM_THREADS=(1 2 4 8 12 16)
