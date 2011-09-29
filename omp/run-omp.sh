c28e0339c3392989906439a80729b0b85c29e84a
uenop <dummy@mail.com>
2011-09-29 16:42:35 +0900

    fix

--- omp/run-omp.sh
+++ omp/run-omp.sh
@@ -1,26 +1,17 @@
 # run-omp
 # @$-eo
 # @$-q sh20100
-# @$-lP 1
-# @$-lp 16
+# @$-lP 16
+# @$-lp 1
 # @$-lm 1800mb
 # @$-cp 0:10:00
 
-cd $QSUB_WORKDIR
-
-#set -x
+PROGS=("enshu_omp" "enshu_omp_with_firsttouch" "enshu_omp_mpilike")
 NUM_THREADS=(1 2 4 8 12 16)
-for i in ${NUM_THREADS[@]}; do
-    OMP_NUM_THREADS=i
-    ./enshu_omp
-done
-echo "----"
-for i in ${NUM_THREADS[@]}; do
-    OMP_NUM_THREADS=i
-    ./enshu_omp_with_firsttouch
-done
-echo "----"
-for i in ${NUM_THREADS[@]}; do
-    OMP_NUM_THREADS=i
-    ./enshu_omp_mpilike
+for prog in ${PROGS[@]}; do
+    echo $prog
+    for i in ${NUM_THREADS[@]}; do
+        OMP_NUM_THREADS=i
+        ./$prog
+    done
 done
