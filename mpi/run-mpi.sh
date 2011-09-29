d9f6b67386436b48bca987a4db1d86845c079cd9
uenop <dummy@mail.com>
2011-09-29 14:36:47 +0900

    fix

--- mpi/run-mpi.sh
+++ mpi/run-mpi.sh
@@ -1,17 +1,18 @@
 #  "# "に"@"と"$"が続く行は、NQSオプションの指定
 # @$-eo
 # @$-q sh20100
+# @$-lP 16
 # @$-lp 1
 # @$-lm 1800mb
 # @$-cp 0:10:00
-# @$-o ret-mpi.txt
 
 cd $QSUB_WORKDIR
 NUM_CPUS=(1 2 4 8 12 16)
 for i in ${NUM_CPUS[@]}; do
-# @$-lP $i
-    for temp in 1..10; do
-        echo mpiexec -n $i a.out
+    count=10
+    while [ $count -gt 0 ]; do
+        mpiexec -n $i a.out
+        count=`expr $count - 1`
     done
 done
 
