800bda99b73973e5552b260c6989da2e3ddc9ada
uenop <dummy@mail.com>
2011-09-29 16:39:21 +0900

    fix

--- omp/run-omp.sh
+++ omp/run-omp.sh
@@ -1,12 +1,11 @@
 # runomp
 #set -x
 PROGS=("enshu_omp" "enshu_omp_with_firsttouch" "enshu_omp_mpilike")
-THREAD_NUMS=(1 2 4 8 12 16)
+NUM_THREADS=(1 2 4 8 12 16)
 for prog in ${PROGS[@]}; do
     echo $prog
-    for i in ${THREAD_NUMS[@]}; do
+    for i in ${NUM_THREADS[@]}; do
         OMP_NUM_THREADS=i
-#        ./$prog
-        echo $i $prog
+        ./$prog
     done
 done
