2b98b78a6308b9762d8fd16c105814227e3d4183
uenop <dummy@mail.com>
2011-09-29 19:23:56 +0900

    fix

--- mpi/enshub_mpi.c
+++ mpi/enshub_mpi.c
@@ -87,12 +87,14 @@ int main(int argc, char** argv){
             }
         }
         /* output: rank_n-1 */
+        if(nrank>0){
         MPI_Recv( &u[0][-1], width*(height-1), MPI_DOUBLE, nrank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
         for(j=0;j<height-1;j++){
             for(i=-1;i<NX;i++)
                 fprintf( udata, "%.15E %.15E %.15E\n", (i+1)*h, ( j+1+(nrank-1)*(height-2) )*h, u[j][i] );
             fprintf( udata, "\n" );
         }
+        }        
         fclose(udata);
     } else if (irank<nrank-1) {
         MPI_Send (&u[0][-1], width*(height-2), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD );
